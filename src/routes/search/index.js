588853504da9fdebbb049ca245100dabdccabe37
JoãoScheuermann <scheuermann.design@gmail.com>
2019-07-10 04:49:23 -0300

    Finished Grid Template for Search route

--- src/routes/search/index.js
+++ src/routes/search/index.js
@@ -1,8 +1,25 @@
+import './style.scss'
 import React from 'react'
 
-export default ({ match }) => {
+import Logo from '../../components/logo'
+import SearchBar from '../../components/search-bar'
+
+export default function SearchRoute ({ match }) {
+
+  console.log(match)
 
   return (
-    <div> { match.params.username } </div>
+    <div className="search-route">
+      <Logo small/>
+      <SearchBar placeholder={match.params.username}/>
+
+      <div className="user-data">
+        asdasd
+      </div>
+
+      <div className="user-repos">
+        asdasd
+      </div>
+    </div>
   )
 }
