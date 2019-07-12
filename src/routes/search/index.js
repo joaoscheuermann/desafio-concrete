9331d3352f16e38132a755e9e4874014216f9f56
João Vitor Scheuermann <joao.scheuermann.amadre@youse.com.br>
2019-07-12 14:13:22 -0300

    Fixed search bug

--- src/routes/search/index.js
+++ src/routes/search/index.js
@@ -14,19 +14,18 @@ export default function SearchRoute ({ match, history }) {
   const status = useSelector(state => state.status)
   const dispatch = useDispatch()
 
-  const search = () => dispatch(fetchGithubUserData(match.params.username))
+  useEffect(() => {
+    search(match.params.username)
+  }, [ match.params.username ])
+
+  const search = (username) => dispatch(fetchGithubUserData(username))
   const statusRenderer = ({sucess, message}) => sucess ?
                                                   (<><UserData/> <UserRepositories/></>) :
                                                   (<Title className="message" type="purple">{ message }</Title>)
 
-  useEffect(() => {
-    search()
-  }, [])
-
   const handleOnSearch = ({value}) => {
     if (value.length === 0) return
     history.push(`/${value}`)
-    search()
   }
 
   const searchBarProps = {
